0c61cf8f024f53f856b3577d329bd70de3470db8
lorossi <mail@lorenzoros.si>
2023-06-06 15:34:06 +0200

    updated launcher

--- launcher.sh
+++ launcher.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd /home/pi/scripts/voltraggio-bot
+# change to the script directory
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+# launch the script in the virtual environment
 venv/bin/python3 voltraggio-bot.py
